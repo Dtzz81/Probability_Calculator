ebcd7a2facb382b283a4c87f68858415a3e4ef75
dtzz81 <denisa.turkova@madetech.com>
2023-03-13 14:31:51 +0000

    working on second test

---
+++ calc/probability_calc.py
@@ -0,0 +1,11 @@
+class Balls:
+    def __init__(self, name, count):
+        self.name = name
+        self.count = count
+
+
+def hat():
+    balls = {"blue":5,
+            }
+
+
--- tests/probability_calc_test.py
+++ tests/probability_calc_test.py
@@ -1,5 +1,28 @@
+import pytest
+
+class Balls:
+    def __init__(self, name):
+        self.name = name
+
+@pytest.fixture
+def selected_balls():
+    return Balls("")
+
+
+@pytest.fixture
+def hat(selected_balls):
+    return [Balls(""), selected_balls]
+
+
+def test_balls_in_hat(selected_balls, hat):
+    assert selected_balls in hat
+
 #There is a hat containing
 #5 blue balls, 4 red balls, 2 green balls.
+def hat():
+    balls = {"blue":5,
+            }
 
-def test_there_is_a_hat(hat):
-    assert hat() is not None
+def there_are_5_blue_balls_in_hat("blue" : 5, hat)
+    assert {"blue" : 5} in hat
+
